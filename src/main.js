66381d9ffb172931ce772bf841e07b9b49eaf1b0
rubtsovi.90 <rubtsovi.90@gmail.com>
2022-07-15 17:23:47 +0200

    init commit

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import { createApp } from "vue";
+import ElementPlus from "element-plus";
+import elementPL from "element-plus/es/locale/lang/pl";
+import dayjs from "dayjs";
+import plLocale from "dayjs/locale/pl";
+import "element-plus/dist/index.css";
+import "./style.css";
+import App from "./App.vue";
+
+dayjs.locale(plLocale, null, false);
+
+const app = createApp(App);
+app.use(ElementPlus, { locale: elementPL });
+app.mount("#app");
